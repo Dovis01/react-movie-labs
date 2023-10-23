0f7b1deb5660b5e593b630935b2639985cc96c2e
Shijin Zhang <1364226740@qq.com>
2023-10-24 03:30:24 +0800

    Updating movie page to use react query

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -11,10 +11,21 @@ export const getMovies = () => {
     });
 };
 
-export const getMovie = id => {
+export const getMovie = (args) => {
+    //console.log(args) -> queryKey JSON
+    const [, idPart] = args.queryKey;
+    const { id } = idPart;
     return fetch(
         `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    ).then(res => res.json());
+    ).then((response) => {
+        if (!response.ok) {
+            throw new Error(response.json().message);
+        }
+        return response.json();
+    })
+        .catch((error) => {
+            throw error
+        });
 };
 
 export const getGenres = async () => {
@@ -33,12 +44,21 @@ export const getGenres = async () => {
         });
 };
 
-export const getMovieImages = (id) => {
+export const getMovieImages = ({ queryKey }) => {
+    const [, idPart] = queryKey;
+    const { id } = idPart;
     return fetch(
         `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    )
-        .then((res) => res.json())
-        .then((json) => json.posters);
+    ).then( (response) => {
+        if (!response.ok) {
+            throw new Error(response.json().message);
+        }
+        return response.json();
+
+    })
+        .catch((error) => {
+            throw error
+        });
 };
 
 export const getMovieReviews = (id) => {
